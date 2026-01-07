9b325ce2353eae9a3b2368b62bd501d3d756376c
孟祥涵 <1056811341@qq.com>
2026-01-07 13:19:47 +0800

    refactor: 移动自动生成类型文件到types目录并清理旧文件

--- src/types/auto-components.d.ts
+++ src/types/auto-components.d.ts
@@ -8,7 +8,6 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    AConfigProvider: typeof import('ant-design-vue/es')['ConfigProvider']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
